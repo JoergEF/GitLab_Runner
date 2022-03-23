11a145d9ecfd426bc3758a347bc8cd4c6d6ee529
Jörg Hofmann <joerg@thuenic.de>
2022-03-23 21:40:57 +0100

    Runner und Docker Provision

--- scripts/runner-provision.sh
+++ scripts/runner-provision.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+cat <<EOF | sudo tee /etc/apt/preferences.d/pin-gitlab-runner.pref
+Explanation: Prefer GitLab provided packages over the Debian native ones
+Package: gitlab-runner
+Pin: origin packages.gitlab.com
+Pin-Priority: 1001
+EOF
+curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+sudo apt-get install gitlab-runner
